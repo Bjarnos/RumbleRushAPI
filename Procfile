de700f93aeccebb9012d2f12e44c6323db6bc1d2
Bjarnos <contact@bjarnos.dev>
2025-09-30 12:41:50 +0200

    Added auto ad watcher

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app:app --host 0.0.0.0 --port $PORT --workers 1
+web: python3 auto.py & uvicorn app:app --host 0.0.0.0 --port $PORT --workers 1
---
+++ auto.py
@@ -0,0 +1,89 @@
+import requests, json, os, dotenv
+from datetime import datetime
+from time import sleep
+
+dotenv.load_dotenv()
+AUTH = os.environ.get('API_KEY')
+
+quest_url = "https://us-central1-pocketrun-33bdc.cloudfunctions.net/v0270_quests/progressQuests"
+purchase_url = "https://us-central1-pocketrun-33bdc.cloudfunctions.net/v0270_purchases/purchase"
+adchest_url = "https://us-central1-pocketrun-33bdc.cloudfunctions.net/v0270_ads/claimAdChest"
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Authorization": f"Bearer {AUTH}",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json",
+    "Host": "us-central1-pocketrun-33bdc.cloudfunctions.net",
+    "Origin": "https://rumblerush.io",
+    "Referer": "https://rumblerush.io/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "TE": "trailers",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0",
+}
+
+relnow = None
+timedheaders = headers.copy()
+timedheaders["variants"] = json.dumps({
+    "General":"0_270_web",
+    "Gameplay":"default",
+    "Cosmetics":"default",
+    "Ranks":"default",
+    "LootBoxes":"default",
+    "Currencies":"default",
+    "Shop":"0_250_web",
+    "WinProgression":"default",
+    "Items":"0_240_web",
+    "PendingRewardUpgrades":"default",
+    "Ads":"default",
+    "TrophyLeaderboard":"default",
+    "NewPlayerGifts":"default",
+    "FriendReferral":"default",
+    "PassConfigs":"0_270_web",
+    "DailyQuests":"default",
+    "Events":"0_270_web"
+})
+
+def update_reltime():
+    global relnow
+    relnow += 30
+    timedheaders["time"] = datetime.fromtimestamp(relnow).strftime("%m/%d/%Y %H:%M:%S")
+
+watched = 0
+def watch_ad(data):
+    global watched
+    update_reltime()
+    r = requests.post(purchase_url, headers=timedheaders, json=data, timeout=60)
+    watched += 1
+    if watched == 3:
+        watched = 0
+        requests.post(adchest_url, headers=timedheaders, timeout=60)
+    return r
+
+#requests.post(quest_url, headers=timedheaders, json={"questsToProgress":{"DailyQuests":{"quests.daily.tuesday.3":1.0}}}) # for real quests
+
+while True:
+    try:
+        sleep(7200)
+        relnow = datetime.now().timestamp()
+
+        print("Chunk 1", flush=True)
+        watch_ad({"purchaseItemId":"uncommon_lootbox.rv","purchaseSource":"Shop"})
+        watch_ad({"purchaseItemId":"pack.freegems.1","purchaseSource":"Shop"})
+        watch_ad({"purchaseItemId":"pack.freecoins.1","purchaseSource":"Shop"})
+
+        print("Chunk 2", flush=True)
+        watch_ad({"purchaseItemId":"uncommon_lootbox.rv","purchaseSource":"Shop"})
+        watch_ad({"purchaseItemId":"pack.freegems.1","purchaseSource":"Shop"})
+        watch_ad({"purchaseItemId":"pack.freecoins.1","purchaseSource":"Shop"})
+
+        print("Chunk 3", flush=True)
+        watch_ad({"purchaseItemId":"items_upgrade_rv","purchaseSource":"RandomizedItemUpgrade"})
+        r = watch_ad({"purchaseItemId":"items_upgrade_rv","purchaseSource":"RandomizedItemUpgrade"})
+        print(f"Done with status {r.status_code}", flush=True)
+    except Exception as e:
+        print(f"Errored: {e}", flush=True)
